153cc0e956eeb8830d53c3cbadc41335baea53eb
Sasha <x6rrl@yandex.ru>
2024-01-14 21:22:57 +1000

    first commit

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const body = document.querySelector('body');
 
     body.addEventListener('mousemove', function(event) {
-        const x = event.clientX / 50;
-        const y = event.clientY / 50;
+        const x = event.clientX / 40;
+        const y = event.clientY / 40;
         headerImg.style.top = `${x}px`
         headerImg.style.left = `-${y}px`
     });
--- js/testFile.js
+++
@@ -1,7 +0,0 @@
-console.log('132');
-
-if(1 > 2) {
-    return true;
-} else {
-    return false;
-}
